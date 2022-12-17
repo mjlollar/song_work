c13b2741d85a64f87812d6801940052a535dc93f
Matthew Lollar <mjlollar1@gmail.com>
2022-12-17 12:05:49 -0600

    Update ESD_outlier_test.R

--- ESD_outlier_test.R
+++ ESD_outlier_test.R
@@ -1,3 +1,9 @@
+#### RUN ESD test on each genotype in genotype colum, will test up to 10 outliers.
+#### Removes greatest number of outliers where test statistic > crtitical value(0.05) below genotype.
+#### Outputs csv with outliers replaced as NA values
+#### Modify where <THIS_STYLE_IS_PRESENT>
+### Last update MJL 12.16.22
+
 library("ggplot2")
 library("ggthemes")
 library("EnvStats")
@@ -7,25 +13,35 @@ library("tidyverse")
 setwd(<YOUR_PATH>)
 df_data <- read.csv(<YOUR_SONG_DATA_CSV>, header=T, as.is=T)
 
-#### RUN ESD test on each genotype in genotype colum, will test up to 10 outliers.
-#### Prints behavior followed by genotype if a significant number of outliers (>=1) detected.
-#### Prints greatest number of outliers where test statistic > crtitical value(0.05) below genotype.
-#### Currently prints information to STDOUT
-
-genos <- as.vector(unique(df_data$genotype))
+#For ESD test, adjust as desired
 alpha = 0.05
 lam = c(1:10)
 R = c(1:10)
 
-for (calls in colnames(df_data[4:length(df_data)])){    ## Script assumes first three columns of input include [genotype,population,chamber]
-  print(calls)
+#requires as a genotype column and first three columns as non-data columns (ex. genotype,population,chamber)
+genos <- as.vector(unique(df_data$genotype))
+phenos <- colnames(df_data[4:length(df_data)])
+
+#function to get distance from mean
+get_diff <- function(x){
+  z = abs(x - mean_set)
+  return(z)}
+
+#get max row possible in df
+max_row = nrow(df_data)
+
+#initialise output dataframe
+data_genotypes <- as.vector(df_data$genotype)
+df_out <- data.frame(genotype=data_genotypes)
+
+#test for outliers within each genotype for each phenotype
+for (calls in phenos){
+  count_out <- vector(length=max_row)
+  ticker = 1
   for (x in genos) {
     y <- df_data[which(df_data$genotype==x), calls]
-    ######ONLINE CODE BLOCK###### (Lines 25-57 copied from: https://www.itl.nist.gov/div898/handbook/eda/section3/eda35h3.htm, R script 'eda35h3.r')
-    ## Generate normal probability plot.
-    #qqnorm(y) ## Uncomment to produce ggplots for each genotype
-    
-    ## Create function to compute the test statistic.
+    og_y_length <- length(y)
+    ######ONLINE CODE BLOCK###### (Lines 44-71 copied from: https://www.itl.nist.gov/div898/handbook/eda/section3/eda35h3.htm)
     rval = function(y){
       ares = abs(y - mean(y))/sd(y)
       df = data.frame(y, ares)
@@ -52,22 +68,48 @@ for (calls in colnames(df_data[4:length(df_data)])){    ## Script assumes first 
       lam[i] = t*(n-i) / sqrt((n-i-1+t**2)*(n-i+1))
       
     }
-    ## Print results.
     newdf = data.frame(c(1:10),R,lam)
     names(newdf)=c("No. Outliers","Test Stat.", "Critical Val.")
     #################################################
-    out_vec <- c()
-    for (z in 1:5){
+    
+    out_vec = 0 #Highest outlier number
+    #Get maximum outlier count
+    for (z in 1:10){
       j = newdf$`Test Stat.`[z]
       k = newdf$`Critical Val.`[z]
       if(j > k){
-        outlier_count <- newdf$`No. Outliers`[z]
-        out_vec <- append(out_vec, outlier_count)
+        out_vec = as.numeric(newdf$`No. Outliers`[z])
       }
     }
-    if (length(out_vec) > 0){
-      print(x)
-      print(out_vec[length(out_vec)])
+    
+    #Replace outliers if detected with NA values in dataset
+    #Find outlier index/indecies if present 
+    if (out_vec > 1)
+      mean_set <- mean(y) #get mean of set
+      y_diff <- unlist(lapply(y, FUN=get_diff)) #calculate distances
+      y_sort <- sort(y_diff, decreasing=TRUE, index.return=TRUE)$ix[1:out_vec]#Get index of highest diffs
+      y <- y[- y_sort] #remove outliers by indexed position
+      out_mean <- mean(y)
+      print(out_mean,quote=FALSE)}
+    else if (out_vec == 1){
+      mean_set <- mean(y)
+      y_diff <- unlist(lapply(y, FUN=get_diff)) #calculate distances
+      y_sort <- sort(y_diff, decreasing=TRUE, index.return=TRUE)$ix[1] #Get index of highest diffs
+      y <- y[-y_sort] #remove outliers by indexed position
+      out_mean <- mean(y)
+      print(out_mean,quote=FALSE)} 
+    else {
+      out_mean <- mean(y)
+      print(out_mean,quote=FALSE)}
+    
+    # Add values to output vector
+    y <- c(y, rep(NA, og_y_length - length(y)))
+    for (d in y){
+      count_out[ticker] <- d
+      ticker = ticker + 1
     }
   }
+  df_out[calls] <- count_out #add corrected phenotype column to output df
 }
+
+write.csv(df_out, row.names=False, file=<YOUR_OUTFILE>) #output df to cwd
